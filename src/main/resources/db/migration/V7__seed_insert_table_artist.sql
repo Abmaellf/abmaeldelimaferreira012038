8c053c013a0fbf7556184b5911ee3a093e0effa1
Abmaellf <abmael.ninha@gmail.com>
2026-01-25 03:58:32 -0400

    feat: add seed inset table artist

---
+++ src/main/resources/db/migration/V7__seed_insert_table_artist.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO artist (name, type, foundation)
+values
+('Serj Tankian', 'SOLO', NULL),
+('Mike Shinoda', 'SOLO', NULL),
+('Michel Teló', 'SOLO', NULL),
+('Guns N’ Roses', 'BANDA', NULL);
