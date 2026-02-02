406347610b31fa9d436ea19c7507d2946615a18b
Abmaellf <abmael.ninha@gmail.com>
2026-02-02 19:27:15 -0400

    feat: implements find All of users

--- src/main/java/select/music/service/user/UserService.java
+++ src/main/java/select/music/service/user/UserService.java
@@ -1,9 +1,9 @@
 package select.music.service.user;
 
-import select.music.dto.user.UserDetailDTO;
 import select.music.dto.user.UserRequestDTO;
 import select.music.dto.user.UserResponseDTO;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
@@ -11,5 +11,6 @@ public interface UserService {
     UserResponseDTO create(UserRequestDTO request);
 
     UserResponseDTO getById(UUID id);
-}
 
+    List<UserResponseDTO> findAll();
+}
--- src/main/java/select/music/service/user/UserServiceImpl.java
+++ src/main/java/select/music/service/user/UserServiceImpl.java
@@ -1,11 +1,9 @@
 package select.music.service.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import select.music.dto.user.UserDetailDTO;
 import select.music.dto.user.UserRequestDTO;
 import select.music.dto.user.UserResponseDTO;
 import select.music.exception.LoginAlreadyExistsException;
@@ -13,6 +11,7 @@ import select.music.exception.UserNotFoundException;
 import select.music.mapper.user.UserMapper;
 import select.music.repository.UserRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -32,7 +31,7 @@ public class UserServiceImpl implements UserService {
 
         var entity = mapper.toEntity(request);
         String encryptedPassword = new BCryptPasswordEncoder().encode(request.password());
-            entity.setPassword(encryptedPassword);
+        entity.setPassword(encryptedPassword);
         var saved = repository.save(entity);
 
         return mapper.toResponse(saved);
@@ -46,5 +45,9 @@ public class UserServiceImpl implements UserService {
                 .orElseThrow(() -> new UserNotFoundException(id));
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<UserResponseDTO> findAll(){
+        return mapper.toListResponse(repository.findAll());
+    }
 }
-
